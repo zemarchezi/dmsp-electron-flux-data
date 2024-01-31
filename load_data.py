770fbcb573dc8ee7e48a7176fd394e5cb9f73295
Jose <jpmarchezi@gmail.com>
2024-01-31 15:25:42 -0500

    read plasma data

--- load_data.py
+++ load_data.py
@@ -2,6 +2,9 @@
 import numpy as np
 import pandas as pd
 from pysatdata.loaders.load import *
+import pytz
+import datetime
+import matplotlib.pyplot as plt
 # %%
 trange_plot=['2010-07-03', '2010-07-05'] # time range for ploting
 trange=['2010-07-02', '2010-07-06'] 
@@ -11,7 +14,22 @@ paramLoadSat = {"satellite": 'dmsp', "probe": 'f17',
 varss_dmsp = load_sat(trange=trange, satellite=paramLoadSat["satellite"],
                      probe=[paramLoadSat["probe"]],
                      instrument=paramLoadSat["instrument"],datatype=paramLoadSat["datatype"], downloadonly=False,
-                     testRemotePath=True,
+                     testRemotePath=False, searchFilesFirst=True,
                      usePandas=False, usePyTplot=True)
 print(varss_dmsp)
 # %%
+quants_electr_dmsp = pytplot.data_quants['ELE_DIFF_ENERGY_FLUX']
+# %%
+flux_dmsp = quants_electr_dmsp.values
+#%%
+time_dt_dmsp = quants_electr_dmsp.coords['time'].values
+#%%
+time_dt_dmsp = [datetime.datetime.fromtimestamp(i, pytz.timezone("UTC")) for i in time_dt_dmsp]
+# %%
+spec = quants_electr_dmsp.coords['spec_bins'].values
+#%%
+v1 = quants_electr_dmsp.coords['v'].values
+# %%
+ener30 = flux_dmsp[:,-1]
+# %%
+## Ssis data for MLT and location.
