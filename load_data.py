780b4a154f96484c2147dffd4353decece97bedb
Jose <jpmarchezi@gmail.com>
2024-02-01 13:43:58 -0500

    load themal plasma data

--- load_data.py
+++ load_data.py
@@ -6,8 +6,8 @@ import pytz
 import datetime
 import matplotlib.pyplot as plt
 # %%
-trange_plot=['2010-07-03', '2010-07-05'] # time range for ploting
-trange=['2010-07-02', '2010-07-06'] 
+trange_plot=['2009-07-03', '2009-07-05'] # time range for ploting
+trange=['2009-07-02', '2009-07-06'] 
 paramLoadSat = {"satellite": 'dmsp', "probe": 'f17',
                 "instrument": 'ssj', "datatype": 'precipitating-electrons-ions'}
 
@@ -33,3 +33,21 @@ v1 = quants_electr_dmsp.coords['v'].values
 ener30 = flux_dmsp[:,-1]
 # %%
 ## Ssis data for MLT and location.
+paramLoadSat = {"satellite": 'dmsp', "probe": 'f17',
+                "instrument": 'ssies', "datatype": 'thermal-plasma'}
+
+varss_dmsp = load_sat(trange=trange, satellite=paramLoadSat["satellite"],
+                     probe=[paramLoadSat["probe"]],
+                     instrument=paramLoadSat["instrument"],datatype=paramLoadSat["datatype"], downloadonly=False,
+                     testRemotePath=True,
+                     usePandas=False, usePyTplot=True)
+print(varss_dmsp)
+# %%
+quants_mlt_dmsp = pytplot.data_quants['mlt']
+quants_mlat_dmsp = pytplot.data_quants['mlat']
+mlt_dmsp = quants_mlt_dmsp.values
+mlat_dmsp = quants_mlat_dmsp.values
+#%%
+time_mlt_dmsp = quants_mlt_dmsp.coords['time'].values
+#%%
+time_mlt_dmsp = [datetime.datetime.fromtimestamp(i, pytz.timezone("UTC")) for i in time_mlt_dmsp]
