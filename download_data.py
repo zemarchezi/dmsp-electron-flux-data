81f1a21ba5212ebb2232f75905b43666664e4ecf
Jose <jpmarchezi@gmail.com>
2024-02-02 10:55:45 -0500

    storms dmsp data download

---
+++ download_data.py
@@ -0,0 +1,120 @@
+#%%
+import numpy as np
+import pandas as pd
+import glob
+import pytplot
+from tqdm import tqdm
+import pickle
+import gc
+from datetime import datetime
+from pysatdata.loaders.load import *
+import pytz
+from scipy import interpolate as interp
+#%%
+def download_read_electron_data(trange, paramLoadSat):
+    pytplot.del_data()
+    varss_dmsp = load_sat(trange=trange, satellite=paramLoadSat["satellite"],
+                     probe=[paramLoadSat["probe"]],
+                     instrument=paramLoadSat["instrument"],datatype=paramLoadSat["datatype"], downloadonly=False,
+                     testRemotePath=False, searchFilesFirst=True,
+                     usePandas=False, usePyTplot=True)
+    try:
+        quants_dmsp = pytplot.data_quants['ELE_DIFF_ENERGY_FLUX']
+
+        flux_dmsp = quants_dmsp.values
+        time_dt_dmsp = quants_dmsp.coords['time'].values
+        time_dt_dmsp = [datetime.datetime.fromtimestamp(i, pytz.timezone("UTC")) for i in time_dt_dmsp]
+        spec = quants_dmsp.coords['spec_bins'].values[0]
+        colss = [str(i) for i in spec]
+
+        return flux_dmsp, time_dt_dmsp, colss
+    except:
+        return None, None, None
+    
+def download_read_plasma_data(trange, paramLoadSat):
+    pytplot.del_data()
+    varss_dmsp = load_sat(trange=trange, satellite=paramLoadSat["satellite"],
+                     probe=[paramLoadSat["probe"]],
+                     instrument=paramLoadSat["instrument"],datatype=paramLoadSat["datatype"], downloadonly=False,
+                     testRemotePath=False, searchFilesFirst=True,
+                     usePandas=False, usePyTplot=True)
+    try:
+        quants_mlt_dmsp = pytplot.data_quants['mlt']
+        quants_mlat_dmsp = pytplot.data_quants['mlat']
+        mlt_dmsp = quants_mlt_dmsp.values
+        mlat_dmsp = quants_mlat_dmsp.values
+
+        time_mlt_dmsp = quants_mlt_dmsp.coords['time'].values
+        time_mlt_dmsp = [datetime.datetime.fromtimestamp(i, pytz.timezone("UTC")) for i in time_mlt_dmsp]
+
+        return mlt_dmsp, mlat_dmsp, time_mlt_dmsp
+    except:
+        return None, None, None
+
+def interpolate_cadence(data, new_time):
+    fl = interp.interp1d((np.arange(0, len(data))), data, bounds_error=False)
+    lnewy = np.linspace(0, len(data), new_time.shape[0])
+    new_data = fl(lnewy)
+    return new_data
+#%%
+
+AUXDAT_PATH = "/home/jmarchezi/research-projects/gic-statistics/auxData"
+OUTPUT_PATH = "/data/output/gic_statistics/outputData"
+
+#%%
+stormList = pd.read_csv(f"{AUXDAT_PATH}/StormList2_manPhStart_NewBeginEnd.csv")
+stormList.rename(columns={"Unnamed: 0": "stormNumber"}, inplace=True)
+# %%
+logs = open("log.txt", "a")
+stormDmspData = []
+probe = "f17"
+for row in tqdm(stormList.iloc[1:2].iterrows()):
+    
+    initial_phase = pd.to_datetime(row[1].loc["new_begining_times"])
+    main_phase = pd.to_datetime(row[1].loc["main_phase"])
+    minimumSymH = pd.to_datetime(row[1].loc["minimumSymH"])
+    end_rec_phase = pd.to_datetime(row[1].loc["new_ending_times"])
+    
+    year_range = [str(y) for y in range(initial_phase.year,end_rec_phase.year+1)]
+    
+    st = f"{initial_phase.year}_{row[1].loc['stormNumber']}"
+    storm_number = row[1].loc['stormNumber']
+
+    ifHSS = row[1].loc["ifHSS"]
+    ifCME = row[1].loc["ifCME"]
+
+    delta_t = end_rec_phase - initial_phase
+
+    trange=[initial_phase.strftime("%Y-%m-%d"), end_rec_phase.strftime("%Y-%m-%d")] 
+    paramLoadSat_ele = {"satellite": 'dmsp', "probe": probe,
+                "instrument": 'ssj', "datatype": 'precipitating-electrons-ions'}
+
+    flux_dmsp, time_dt_dmsp, colss = download_read_electron_data(trange, paramLoadSat_ele)
+
+    paramLoadSat_plasma = {"satellite": 'dmsp', "probe": probe,
+                "instrument": 'ssies', "datatype": 'thermal-plasma'}
+    
+    mlt_dmsp, mlat_dmsp, time_mlt_dmsp = download_read_plasma_data(trange, paramLoadSat_plasma)
+
+    if flux_dmsp is not None and mlt_dmsp is not None:
+        dfEnergy = pd.DataFrame(flux_dmsp, columns=colss, index=time_dt_dmsp)
+        mlt_new = interpolate_cadence(mlt_dmsp, time_dt_dmsp)
+        mlat_new = interpolate_cadence(mlat_dmsp, time_dt_dmsp)
+        dfEnergy["mlt"] = mlt_new
+        dfEnergy["mlat"] = mlat_new
+        stormDmspData.append(dfEnergy)
+    try:
+        dmspData = pd.concat(stormDmspData)
+
+        dmspData.reset_index(inplace=True)
+
+        dmspData.to_feather(f'{OUTPUT_PATH}/{st}_Dmsp_{probe}_for_storms.feather')
+            
+        logs.write(f'{st} -- ok \n')
+    except Exception as e:
+        print(e)
+
+        logs.write(f'{st} -- ERROR \n')
+    gc.collect()
+logs.close()
+# %%
--- load_data.py
+++ load_data.py
@@ -5,6 +5,7 @@ from pysatdata.loaders.load import *
 import pytz
 import datetime
 import matplotlib.pyplot as plt
+from scipy import interpolate as interp
 # %%
 trange_plot=['2009-07-03', '2009-07-05'] # time range for ploting
 trange=['2009-07-02', '2009-07-06'] 
@@ -26,7 +27,7 @@ time_dt_dmsp = quants_electr_dmsp.coords['time'].values
 #%%
 time_dt_dmsp = [datetime.datetime.fromtimestamp(i, pytz.timezone("UTC")) for i in time_dt_dmsp]
 # %%
-spec = quants_electr_dmsp.coords['spec_bins'].values
+spec = quants_electr_dmsp.coords['spec_bins'].values[0]
 #%%
 v1 = quants_electr_dmsp.coords['v'].values
 # %%
@@ -51,3 +52,48 @@ mlat_dmsp = quants_mlat_dmsp.values
 time_mlt_dmsp = quants_mlt_dmsp.coords['time'].values
 #%%
 time_mlt_dmsp = [datetime.datetime.fromtimestamp(i, pytz.timezone("UTC")) for i in time_mlt_dmsp]
+
+#%%
+
+fl = interp.interp1d((np.arange(0, len(mlt_dmsp))), mlt_dmsp, bounds_error=False)
+lnewy = np.linspace(0, len(mlt_dmsp), ener30.shape[0])
+mlt_new = fl(lnewy)
+# mltDf = pd.DataFrame(mlt_new,index=time).interpolate()
+# %%
+fl = interp.interp1d((np.arange(0, len(mlat_dmsp))), mlat_dmsp, bounds_error=False)
+lnewy = np.linspace(0, len(mlat_dmsp), ener30.shape[0])
+mlat_new = fl(lnewy)
+# %%
+dfEnergy = pd.DataFrame({"ener30": ener30, "mlt": mlt_new, "mlat": mlat_new}, index=time_dt_dmsp)
+# %%
+# mlatArray = dfEnergy["mlat"].apply(np.floor).unique()
+# mlatArray = np.sort(mlatArray)
+bin_size = 3
+mlatArray = np.arange(60,90,bin_size)
+
+mltArray = np.arange(0,24, 1)
+
+# %%
+binOccurrence = np.zeros((len(mltArray),len(mlatArray)))
+for mlat in range(len(mlatArray)):
+    for mlt in range(len(mltArray)):
+        mlst = dfEnergy[(dfEnergy.mlt >= mltArray[mlt]) & (dfEnergy.mlt < mltArray[mlt]+1)]
+        ls = mlst[(mlst['mlat'] >= mlatArray[mlat]) & (mlst['mlat'] < mlatArray[mlat]+bin_size)]
+        binOccurrence[mlt,mlat] = ls.mlat.count() 
+# %%
+rad = mlatArray
+azm = np.asarray(mltArray)/23.*2*np.pi
+r, th = np.meshgrid(rad, azm)
+
+cm2 = plt.cm.get_cmap('plasma')
+fig = plt.figure(figsize=(12,15))
+ax = fig.add_subplot(111, projection='polar')
+ax.set_theta_offset(np.pi*3/2)
+plt.pcolormesh(th, r, binOccurrence, cmap=cm2)
+plt.plot(azm, r, ls='none')
+plt.grid()
+# plt.thetagrids([theta * 15 for theta in range(360//15)], labels=mltArray)
+plt.colorbar(aspect=20, fraction=0.08)
+# # %%
+
+# %%
